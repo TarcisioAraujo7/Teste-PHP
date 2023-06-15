b570ed11c3485ce6025c2852eeeee69edfbd328e
Tarcisio Almeida <tarcisio.olv@gmail.com>
2023-06-15 00:41:13 -0300

    Adicionando API Rest JSON

---
+++ app/Http/Controllers/API/ClientAPIController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Client;
+
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClientAPIController extends Controller
+{
+    public function index() {
+
+
+        $clientes = Client::all(); // Busca todos os clientes do banco de dados
+
+        return response()->json($clientes);
+        
+    }
+
+    public function show($client_id){
+        $client = Client::find($client_id);
+
+        if ($client) {
+            return response()->json($client);
+        } else {
+            return response()->json(['message'=>"cliente não encontrado"], 404);
+        }
+
+    }
+
+    public function consult(Request $request)
+    {
+        $id_client = $request->input('id_client');
+        $clientes = [];
+
+        if ($id_client) {
+            $client = Client::find($id_client);
+
+            if ($client) {
+                $clientes[] = $client;
+            }
+        }
+
+        $show_all = false;
+
+        return view('clients.viewer', compact('show_all', 'clientes'));
+
+    }
+
+    public function show_all()
+    {
+        $show_all = true;
+        $clientes = Client::all();
+
+        return view('clients.viewer', compact('show_all', 'clientes'));
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:20|regex:/^[^0-9]+$/|alpha_dash',
+            'surname' => 'required|max:20|regex:/^[^0-9]+$/|alpha_dash',
+            'cpf' => 'required|regex:/^[0-9]+$/|size:11|unique:clients',
+            'email' => 'email|unique:clients',
+        ], ClientAPIController::messages());
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $client = new Client();
+        $client->name = $request->name;
+        $client->surname = $request->surname;
+        $client->cpf = $request->cpf;
+        $client->email = $request->email;
+
+        $client->save();
+
+        return response()->json($client, 201);
+    }
+
+    public function update(Request $request, $client_id){
+        $client = Client::find($client_id);
+
+        if (!$client) {
+            return response()->json(['message'=>"cliente não encontrado"], 404);
+        } 
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'max:20|regex:/^[^0-9]+$/|alpha_dash',
+            'surname' => 'max:20|regex:/^[^0-9]+$/|alpha_dash',
+            'cpf' => 'regex:/^[0-9]+$/|size:11|unique:clients',
+            'email' => 'email|unique:clients',
+        ], ClientAPIController::messages());
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+        
+        $client->update($request->all());
+
+        return response()->json($client,201);
+
+    }
+
+    public function destroy($client_id){
+        $client = Client::find($client_id);
+
+        if (!$client) {
+            return response()->json(['message'=>"cliente não encontrado"], 404);
+        } 
+
+        $client->delete();
+
+        return response()->json(['message'=>"cliente deletado com sucesso"],201);
+
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => "O campo 'name' é obrigatório.",
+            'name.max' => 'O campo name não pode exceder 20 caracteres.',
+            'name.regex' => 'O campo name não deve conter numeros.',
+            'name.alpha_dash' => 'O campo name deve conter apenas letras.',
+            'surname.required' => "O campo 'surname' é obrigatório.",
+            'surname.max' => 'O campo surname não pode exceder 20 caracteres.',
+            'surname.regex' => 'O campo surname não deve conter numeros.',
+            'surname.alpha_dash' => 'O campo surname deve conter apenas letras.',
+            'cpf.required' => "O campo 'cpf' é obrigatório.",
+            'cpf.regex' => 'O campo CPF deve conter apenas números.',
+            'cpf.size' => 'O campo CPF deve conter 11 caracteres.',
+            'cpf.unique' => 'O CPF informado já está cadastrado.',
+            'email.email' => 'O campo email deve ser um endereço de e-mail válido.',
+            'email.unique' => 'O email informado já está cadastrado.',
+        ];
+
+    }
+
+}
---
+++ app/Http/Controllers/API/ProductAPIController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Product;
+
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductAPIController extends Controller
+{
+    public function index() {
+
+        $products = Product::all();
+        
+        return response()->json($products);
+    }
+
+    public function show($product_id){
+        $product = Product::find($product_id);
+
+        if ($product) {
+            return response()->json($product);
+        } else {
+            return response()->json(['message'=>"produto não encontrado"], 404);
+        }
+
+    }
+
+    public function register(){
+        return view('products.register');
+    }
+
+    public function store(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'name_product' => 'required|max:30',
+            'unitary_value' => 'required|numeric|gt:0|regex:/^\d+(\.\d{1,2})?$/',
+            'bar_code' => 'required|regex:/^[0-9]+$/|size:10|unique:products',
+        ], ProductAPIController::messages());
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $product = new Product();
+        $product->name_product = $request->name_product;
+        $product->unitary_value = $request->unitary_value;
+        $product->bar_code = $request->bar_code;
+
+        $product->save();
+
+        return response()->json($product, 201);
+    }
+
+    public function update(Request $request, $product_id){
+        $product = Product::find($product_id);
+
+        if (!$product) {
+            return response()->json(['message'=>"produto não encontrado"], 404);
+        } 
+
+        $validator = Validator::make($request->all(),[
+            'name_product' => 'max:30',
+            'unitary_value' => 'numeric|gt:0|regex:/^\d+(\.\d{1,2})?$/',
+            'bar_code' => 'regex:/^[0-9]+$/|size:10|unique:products',
+        ], ProductAPIController::messages());
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $product->update($request->all());
+
+        return response()->json($product,201);
+
+    }
+
+    public function destroy($product_id){
+        $product = Product::find($product_id);
+
+        if (!$product) {
+            return response()->json(['message'=>"produto não encontrado"], 404);
+        } 
+
+        $product->delete();
+
+        return response()->json(['message'=>"produto deletado com sucesso"],201);
+
+    }
+
+    public function messages() 
+    {
+        return [
+            'name_product.required' => "O campo 'name_product' é obrigatório.",
+            'name_product.max' => 'O campo nome do produto não pode exceder 30 caracteres.',
+            'unitary_value.required' => "O campo 'unitary_value' é obrigatório.",
+            'unitary_value.numeric' => 'O campo valor unitário deve ser um número.',
+            'unitary_value.gt' => 'O campo valor unitário deve ser maior que 0.',
+            'unitary_value.regex' => 'O campo valor unitário deve ter até 2 casas decimais.',
+            'bar_code.required' => "O campo 'bar_code' é obrigatório.",
+            'bar_code.regex' => 'O campo código de barras deve conter apenas números.',
+            'bar_code.size' => 'O campo código de barras deve ter exatamente 10 caracteres.',
+            'bar_code.unique' => 'O código de barras informado já está cadastrado.',
+        ];
+    }
+}
