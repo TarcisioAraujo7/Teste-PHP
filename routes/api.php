31ccd1182178b37667bb1061b0acc88da0e93bfa
Tarcisio Almeida <tarcisio.olv@gmail.com>
2023-06-15 00:47:15 -0300

    Rotas alteradas

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\OrderController;
+use App\Http\Controllers\API\ClientAPIController;
+use App\Http\Controllers\API\ProductAPIController;
+use App\Http\Controllers\API\OrderAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,31 +25,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('clients')->group(function () {
 
-    Route::get('/', [ClientController::class, 'index']);
-    Route::post('/', [ClientController::class, 'store']);
-    Route::get('/{id}', [ClientController::class, 'show']);
-    Route::put('/{id}', [ClientController::class, 'update']);
-    Route::delete('/{id}', [ClientController::class, 'destroy']);
+    Route::get('/', [ClientAPIController::class, 'index']);
+    Route::post('/', [ClientAPIController::class, 'store']);
+    Route::get('/{id}', [ClientAPIController::class, 'show']);
+    Route::put('/{id}', [ClientAPIController::class, 'update']);
+    Route::delete('/{id}', [ClientAPIController::class, 'destroy']);
 
 });
 
 Route::prefix('products')->group(function () {
 
-    Route::get('/', [ProductController::class, 'index']);
-    Route::post('/', [ProductController::class, 'store']);
-    Route::get('/{id}', [ProductController::class, 'show']);
-    Route::put('/{id}', [ProductController::class, 'update']);
-    Route::delete('/{id}', [ProductController::class, 'destroy']);
+    Route::get('/', [ProductAPIController::class, 'index']);
+    Route::post('/', [ProductAPIController::class, 'store']);
+    Route::get('/{id}', [ProductAPIController::class, 'show']);
+    Route::put('/{id}', [ProductAPIController::class, 'update']);
+    Route::delete('/{id}', [ProductAPIController::class, 'destroy']);
 
 });
 
 Route::prefix('orders')->group(function () {
 
-    Route::get('/', [OrderController::class, 'index']);
-    Route::post('/', [OrderController::class, 'store']);
-    Route::get('/{id}', [OrderController::class, 'show']);
-    Route::put('/{id}', [OrderController::class, 'update']);
-    Route::delete('/{id}', [OrderController::class, 'destroy']);
+    Route::get('/', [OrderAPIController::class, 'index']);
+    Route::post('/', [OrderAPIController::class, 'store']);
+    Route::get('/{id}', [OrderAPIController::class, 'show']);
+    Route::put('/{id}', [OrderAPIController::class, 'update']);
+    Route::delete('/{id}', [OrderAPIController::class, 'destroy']);
 
 });
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,24 +22,68 @@ Route::get('/', function () {
 
 
 Route::prefix('clients')->group(function () {
+
     Route::get('/', [ClientController::class, 'index']);
-    Route::post('/', [ClientController::class, 'store']);
-
-
     Route::post('/exibir-todos', [ClientController::class, 'show_all'])->name('clients.show_all');
-    Route::post('/', [ClientController::class, 'consult'])->name('clients.consult');
+    Route::post('/exibir_client', [ClientController::class, 'show']);
 
-    Route::get('/{id}', [ClientController::class, 'show']);
+    Route::get('/cadastrar', [ClientController::class, 'form_post']);
+    Route::post('/', [ClientController::class, 'store'])->name('clients.add');
+
+    Route::get('/atualizar', [ClientController::class, 'selec_put']);
+    Route::get('/atualizar/form/', [ClientController::class, 'form_put']);
     Route::put('/{id}', [ClientController::class, 'update']);
+
+    Route::get('/deletar', [ClientController::class, 'selec_del']);
+    Route::get('/deletar/mass', [ClientController::class, 'selec_massdel']);
+    Route::delete('/deletar/mass', [ClientController::class, 'massdel']);
+    Route::get('/deletar/confirma', [ClientController::class, 'confirm_destroy']);
     Route::delete('/{id}', [ClientController::class, 'destroy']);
 
 });
 
+Route::prefix('products')->group(function () {
 
-// Route::get('/clients', [ClientController::class, 'index']);
-// Route::get('/clients/{client_id}', [ClientController::class, 'read_one'])->where('client_id','[0-9]+');
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/exibir-todos', [ProductController::class, 'show_all'])->name('products.show_all');
+    Route::post('/exibir_product', [ProductController::class, 'show'])->name('product.show');
 
-// Route::post('/addclient', [ClientController::class, 'store']);
-// Route::get('/clients/register', [ClientController::class, 'register']);
+    Route::get('/cadastrar', [ProductController::class, 'form_post']);
+    Route::post('/', [ProductController::class, 'store'])->name('products.add');
 
+    Route::get('/atualizar', [ProductController::class, 'selec_put']);
+    Route::get('/atualizar/form/', [ProductController::class, 'form_put']);
+    Route::put('/{id}', [ProductController::class, 'update']);
 
+    Route::get('/deletar', [ProductController::class, 'selec_del']);
+    Route::get('/deletar/mass', [ProductController::class, 'selec_massdel']);
+    Route::delete('/deletar/mass', [ProductController::class, 'massdel']);
+    Route::get('/deletar/confirma', [ProductController::class, 'confirm_destroy']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+
+    Route::get('/promo', [ProductController::class, 'selec_promo']);
+    Route::get('/promo/confirma', [ProductController::class, 'confirm_promo']);
+    Route::put('/promo/{id}', [ProductController::class, 'update_value']);
+
+});
+
+Route::prefix('orders')->group(function () {
+
+    Route::get('/', [OrderController::class, 'index']);
+    Route::post('/exibir-todos', [OrderController::class, 'showAll'])->name('products.showAll');
+    Route::post('/exibir_order', [OrderController::class, 'show'])->name('product.show');
+
+    Route::get('/cadastrar', [OrderController::class, 'form_post']);
+    Route::post('/', [OrderController::class, 'store'])->name('products.add');
+
+    Route::get('/atualizar', [OrderController::class, 'selec_put']);
+    Route::get('/atualizar/form/', [OrderController::class, 'form_put']);
+    Route::put('/{id}', [OrderController::class, 'update']);
+
+    Route::get('/deletar', [OrderController::class, 'selec_del']);
+    Route::get('/deletar/mass', [OrderController::class, 'selec_massdel']);
+    Route::delete('/deletar/mass', [OrderController::class, 'massdel']);
+    Route::get('/deletar/confirma', [OrderController::class, 'confirm_destroy']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
+
+});
